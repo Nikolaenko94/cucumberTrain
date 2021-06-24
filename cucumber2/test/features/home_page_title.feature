f37d1914f6c3b514d0dee388d060c0c2a73052fa
Stanislav Nikolaenko <Stanislav_Nikolaenko@epam.com>
2021-06-24 13:03:57 +0300

    feat: add not necessary variables for page title test. Used (not )? configaration

--- cucumber2/test/features/home_page_title.feature
+++ cucumber2/test/features/home_page_title.feature
@@ -4,6 +4,7 @@ Feature: Home Page Title
 Scenario: Page title
     Given I open "https://www.epam.com/" url
     Then Page title should be "EPAM | Enterprise Software Development, Design & Consulting"
+        And Page title should not be "EPAM | "
     When I wait "3" seconds  
 
 Scenario: Page title
--- cucumber2/test/step_definitions/browser_steps.js
+++ cucumber2/test/step_definitions/browser_steps.js
@@ -1,16 +1,22 @@
 const { When, Then, setDefaultTimeout } = require('cucumber');
 const { expect } = require('chai');
-const { browser } = require('protractor');
+
 
 setDefaultTimeout(60000);
 
 When(/^I open "([^"]*)" url$/,function(url){
     return browser.get(url);
 });
-Then(/^Page title should be "([^"]*)"$/, async function(title){
-    const pageTitle = await browser.getTitle();
-    expect(pageTitle).to.be.equal(title); 
+
+Then(/^Page title should (not )?be "([^"]*)"$/, async function(notArg, title){
+    const pageTitle = await browser.getTitle();    
+    if(notArg){
+        expect(pageTitle).to.not.be.equal(title);  
+    } else{
+        expect(pageTitle).to.be.equal(title); 
+    };         
 });
+
 When(/^I wait "([^"]*)" seconds$/, function(seconds){
     return browser.sleep(seconds * 1000);
-});
+}); 
