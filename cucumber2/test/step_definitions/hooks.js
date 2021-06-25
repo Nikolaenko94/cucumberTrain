914722ed9bc836abb990ff329609d17f650045a9
Stanislav Nikolaenko <Stanislav_Nikolaenko@epam.com>
2021-06-25 10:16:48 +0300

    feat: demonstration hook only for one tag

--- cucumber2/test/step_definitions/hooks.js
+++ cucumber2/test/step_definitions/hooks.js
@@ -1,6 +1,14 @@
 const { After } = require("cucumber");
 
 //this hook needed for add screenshots after each test
+//if used After({tags: "@tagsName"}, func(){}) - we apply this only for this tag
+// After({tags: "@so"},async function(){
+//     const screenshot = await browser.takeScreenshot();
+//     //conver img from base_64 to png
+//     const decodedImage = new Buffer.from(screenshot, 'base64');
+//     //at this case, 'this' - show our scenarios
+//     return this.attach(decodedImage, 'image/png');
+// })
 After(async function(){
     const screenshot = await browser.takeScreenshot();
     //conver img from base_64 to png
